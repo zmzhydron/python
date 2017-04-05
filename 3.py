c4362dd2cde93028d8f6f0b23081eda6ee89bd3e
zmzhydron <zmzhydron@Outlook.com>
2017-04-05 17:57:42 +0800

    sdf

--- 3.py
+++ 3.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # day3
 
+import sys
+import PIL
+
 lista = ['zmz']+[ 'index is '+ str(index)+'value is:'+str(value) for index, value in enumerate(range(0,100)) if index%2 == 0][20:]
 print(lista)
+# print(sys.path)
