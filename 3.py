a39b54630da58363ea0090e744f03dc1c63c9c42
zmzhydron <zmzhydron@outlook.com>
2017-04-06 21:46:51 +0800

    sdfa

--- 3.py
+++ 3.py
@@ -5,5 +5,20 @@ import sys
 import PIL
 
 lista = ['zmz']+[ 'index is '+ str(index)+'value is:'+str(value) for index, value in enumerate(range(0,100)) if index%2 == 0][20:]
-print(lista)
+# print(lista)
 # print(sys.path)
+print("paper bich")
+def main():
+  count = 0
+  while(count < 20):
+    count = count + 1
+    yield count
+
+
+if __name__ == '__main__':
+  print("shit man %s"%__name__)
+  gena = (x for x in range(0,20))
+  for s in gena:
+    print(s)
+else:
+  print("gogobich")
