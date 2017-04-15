4f8e5271f6e04078d55ac782669ebf4f0771d816
zmzhydron <zmzhydron@outlook.com>
2017-04-15 13:54:38 +0800

    asdfvg

--- 4-15.py
+++ 4-15.py
@@ -6,20 +6,22 @@ import PIL
 #decorator
 
 def log(value):
-  def shit(fn):
+  def shit(fn,fnname="fuckyou"):
     print("this is log function")
     def wrapper(*arg,**kw):
       print("fuckyou bitch : %s"%value)
       fn(*arg,**kw)
+      wrapper.__name__ = fn.__name__
       print("cumming into your pussy : %s"%value)
     return wrapper
   return shit
 
 @log("alice")
-def myname(name='zhangmingzhi'):
+def mynames(name='zhangmingzhi'):
   print("hello my name is %s"%name)
 
-myname('sjb');
+mynames('sjb');
+print(mynames.__name__)
 
 def argumentsTest(a,b,c='c',*l,**kw):
   print("a:%s, b:%s, c%s"%(a,b,c))
